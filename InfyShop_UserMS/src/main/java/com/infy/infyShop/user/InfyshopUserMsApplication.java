f971984af0e6093b00fcedd051f257c4b97900f7
khushboo <khushboo.jethani@infosys.com>
2021-05-20 13:14:06 +0530

    Added UserMS

---
+++ InfyShop_UserMS/src/main/java/com/infy/infyShop/user/InfyshopUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.infy.infyShop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyshopUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyshopUserMsApplication.class, args);
+	}
+
+}
